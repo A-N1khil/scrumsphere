cccad3b1ff6ed75eb96a088a5187bf4c42f8a6a7
A-N1khil <nikhilanand1006@gmail.com>
2024-12-21 18:19:14 -0500

    Npm build local working

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import eslint from 'vite-plugin-eslint'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint({
+    emitWarning: true,
+    emitError: false
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
