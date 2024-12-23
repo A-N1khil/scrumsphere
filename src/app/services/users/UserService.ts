85fb5fde1954b4c65bbb51651797ff3db25cc976
A-N1khil <nikhilanand1006@gmail.com>
2024-12-22 20:41:25 -0500

    Add user service

---
+++ src/app/services/users/UserService.ts
@@ -0,0 +1,24 @@
+import { User } from "@/app/models/users/User";
+import { httpService } from "../http/HttpService";
+
+class UserService {
+  async login(userId: string, password: string): Promise<User> {
+    return await httpService.post("/users/login", { userId, password }, false).then((response) => {
+      return response.data as User;
+    });
+  }
+
+  async isUserNameAvaiable(userId: string): Promise<boolean> {
+    return await httpService.get(`/users/checkUserId`, { userId }, false).then((response) => {
+      return response.data as boolean;
+    });
+  }
+
+  async register(user: User): Promise<User> {
+    return await httpService.post("/users/register", user, false).then((response) => {
+      return response.data as User;
+    });
+  }
+}
+
+export const userService = new UserService();
