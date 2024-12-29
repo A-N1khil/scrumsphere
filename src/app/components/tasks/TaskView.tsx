d11a4f49999d84fa5266b4a955fc05776c635940
A-N1khil <nikhilanand1006@gmail.com>
2024-12-29 14:16:49 +0530

    Completed TaskView

--- src/app/components/tasks/TaskView.tsx
+++ src/app/components/tasks/TaskView.tsx
@@ -35,6 +35,34 @@ const TaskView = ({ task }: TaskViewProps) => {
                   {task.title}
                 </h1>
                 <p>{task.description}</p>
+                <Separator className="mt-2 border-foreground border-1 max-w-lg" />
+                <div className="mt-1 grid grid-cols-2">
+                  <div className="flex flex-col items-start col-span-1">
+                    <div className="flex flex-row justify-center items-center">
+                      Assigned to:
+                      <CircleUser size={18} className="mr-1 ml-2" /> {task.assignee}
+                    </div>
+                    <div className="flex flex-row justify-center items-center">
+                      Reported by
+                      <UserPen size={18} className="mr-1 ml-2" /> {task.reporter}
+                    </div>
+                  </div>
+                  <div className="flex flex-col items-start col-span-1">
+                    <div className="grid grid-cols-2 flex flex-row justify-start items-start">
+                      <div className="col-span-1">Due</div>
+                      <div className="col-span-1 ml-2 flex flex-row justify-center items-center">
+                        <CalendarPlus size={18} className="mr-1" /> {task.dueDate}
+                      </div>
+                    </div>
+                    <div className="grid grid-cols-2 flex flex-row justify-start">
+                      <div className="col-span-1">Last updated:</div>
+                      <div className="col-span-1 ml-2 flex flex-row justify-start items-center">
+                        <CalendarArrowUp size={18} className="mr-1" /> {task.updatedDate}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <Separator className="mt-2 border-foreground border-1 max-w-lg" />
               </div>
             </div>
             <div className="col-span-1">
@@ -49,41 +77,8 @@ const TaskView = ({ task }: TaskViewProps) => {
           <div className="flex flex-row items-center justify-end"></div>
         </CardHeader>
         <CardContent>
-          <div className="grid grid-cols-4 gap-4">
-            <div className="col-span-3 text-left items-start">
-              <MarkdownEditor markdown={task.description} postMessage={getNewMessage} />
-            </div>
-            <div className="col-span-1">
-              <div className="flex flex-col items-start">
-                <div className="grid grid-cols-2 flex flex-row">
-                  <div className="col-span-1">Assigned to:</div>
-                  <div className="col-span-1 ml-2 flex flex-row justify-center items-center">
-                    <CircleUser size={18} className="mr-1" /> {task.assignee}
-                  </div>
-                </div>
-                <div className="grid grid-cols-2 flex flex-row">
-                  <div className="col-span-1">Reporter:</div>
-                  <div className="col-span-1 ml-2 flex flex-row justify-center items-center">
-                    <UserPen size={18} className="mr-1" /> {task.reporter}
-                  </div>
-                </div>
-                <Separator className="mt-2 border-foreground border-1" />
-              </div>
-              <div className="flex flex-col items-start">
-                <div className="mt-3 grid grid-cols-2 flex flex-row justify-start items-start">
-                  <div className="col-span-1">Due</div>
-                  <div className="col-span-1 ml-2 flex flex-row justify-center items-center">
-                    <CalendarPlus size={18} className="mr-1" /> {task.dueDate}
-                  </div>
-                </div>
-                <div className="grid grid-cols-2 flex flex-row justify-start">
-                  <div className="col-span-1">Last updated:</div>
-                  <div className="col-span-1 ml-2 flex flex-row justify-start items-center">
-                    <CalendarArrowUp size={18} className="mr-1" /> {task.updatedDate}
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="text-left items-start">
+            <MarkdownEditor markdown={task.description} postMessage={getNewMessage} />
           </div>
         </CardContent>
         <CardFooter className="flex flex-col justify-start items-start">
