7044d3056eefda91525e8405f6f90555e8d41e89
A-N1khil <nikhilanand1006@gmail.com>
2024-12-31 13:57:44 +0530

    TC passing

--- cypress/e2e/ModalOpenWorkflow.cy.ts
+++ cypress/e2e/ModalOpenWorkflow.cy.ts
@@ -3,12 +3,12 @@ describe("Modal open workflow", () => {
     cy.visit("/tasks");
   });
   it("should have tasks list populated", () => {
-    cy.get('[data-testid="TASK-1"]').should("exist");
+    cy.get('[data-testid="676eeddaf5264633ed8421ba"]').should("exist");
   });
   it("should open modal on task click", () => {
     cy.get('[data-testid="modal-TASK-1"')
       .should("exist")
       .click()
-      .then(() => cy.get('[data-testId="edit-task-button"]').should("exist"));
+      .then(() => cy.get('[data-testid="edit-task-button"]').should("exist"));
   });
 });
